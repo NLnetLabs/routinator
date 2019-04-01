bd29cf649a0da1f8743a9eca1caecda2da3e41c2
Martin Hoffmann <hn@nvnc.de>
2019-04-01 11:38:44 +0200

    Check for minimum Rust version before building.

---
+++ build.rs
@@ -0,0 +1,14 @@
+extern crate rustc_version;
+use rustc_version::{Version, version};
+
+fn main() {
+    let version = version().expect("Failed to get rustc version.");
+    if version < Version::parse("1.30.0").unwrap() {
+        eprintln!(
+            "\n\nAt least Rust version 1.30 is required.\n\
+             Version {} is used for building.\n\
+             Build aborted.\n\n",
+             version);
+        panic!();
+    }
+}
