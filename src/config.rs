5a14d4619de41b1191a4f5e2997f724faff9a6cc
Martin Hoffmann <hn@nvnc.de>
2020-06-08 18:30:22 +0200

    Add an alias for the misspelled command.

--- src/config.rs
+++ src/config.rs
@@ -260,6 +260,7 @@ impl Config {
         )
         .arg(Arg::with_name("allow-dubious-hosts")
              .long("allow-dubious-hosts")
+             .aliases(&["allow-dubios-hosts"])
              .help("Allow dubious host names in rsycn and HTTPS URIs")
         )
         .arg(Arg::with_name("disable-rsync")
