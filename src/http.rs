55671c3417041e4adb989d072681a9742db0c769
Martin Hoffmann <martin@nlnetlabs.nl>
2021-01-25 12:19:43 +0100

    Introducing: the Routinator UI.

--- src/http.rs
+++ src/http.rs
@@ -143,6 +143,9 @@ async fn handle_request(
         path if path.starts_with("/api/v1/validity/") => {
             validity_path(origins, &path[17..])
         }
+        #[cfg(feature = "ui")]
+        _ => self::ui::process_request(req),
+        #[cfg(not(feature = "ui"))]
         _ => not_found()
     })
 }
@@ -1008,4 +1011,101 @@ impl Drop for HttpStream {
     }
 }
 
+
+//============ Routinator UI =================================================
+
+#[cfg(feature = "ui")]
+mod ui {
+    use hyper::{Body, Request, Response};
+
+    macro_rules! assets {
+        (
+            $(
+                ( $path:expr => $( $ext:ident ),*  ),
+            )*
+        )
+        => {
+            pub fn process_request(req: Request<Body>) -> Response<Body> {
+                match req.uri().path() {
+                    "/" => {
+                        serve(
+                            include_bytes!(
+                                "../contrib/routinator-ui/index.html"
+                            ),
+                            self::ctypes::html
+                        )
+                    }
+                    $(
+                        $(
+                            concat!("/", $path, ".", stringify!($ext)) => {
+                                serve(
+                                    include_bytes!(
+                                        concat!(
+                                            "../contrib/routinator-ui/",
+                                            $path, ".",
+                                            stringify!($ext)
+                                        )
+                                    ),
+                                    self::ctypes::$ext
+                                )
+                            }
+                        )*
+                    )*
+                    _ => super::not_found()
+                }
+            }
+        }
+    }
+
+    assets!(
+        ("favicon" => ico),
+        ("css/app" => css),
+        ("fonts/element-icons" => ttf, woff),
+        ("fonts/lato-latin-100" => woff, woff2),
+        ("fonts/lato-latin-300" => woff, woff2),
+        ("fonts/lato-latin-400" => woff, woff2),
+        ("fonts/lato-latin-700" => woff, woff2),
+        ("fonts/lato-latin-900" => woff, woff2),
+        ("fonts/lato-latin-100italic" => woff, woff2),
+        ("fonts/lato-latin-300italic" => woff, woff2),
+        ("fonts/lato-latin-400italic" => woff, woff2),
+        ("fonts/lato-latin-700italic" => woff, woff2),
+        ("fonts/lato-latin-900italic" => woff, woff2),
+        ("fonts/source-code-pro-latin-200" => woff, woff2),
+        ("fonts/source-code-pro-latin-200" => woff, woff2),
+        ("fonts/source-code-pro-latin-300" => woff, woff2),
+        ("fonts/source-code-pro-latin-400" => woff, woff2),
+        ("fonts/source-code-pro-latin-500" => woff, woff2),
+        ("fonts/source-code-pro-latin-600" => woff, woff2),
+        ("fonts/source-code-pro-latin-700" => woff, woff2),
+        ("fonts/source-code-pro-latin-900" => woff, woff2),
+        ("img/afrinic" => svg),
+        ("img/apnic" => svg),
+        ("img/arin" => svg),
+        ("img/lacnic" => svg),
+        ("img/ripencc" => svg),
+        ("img/routinator_logo_white" => svg),
+        ("img/welcome" => svg),
+        ("js/app" => js),
+    );
+
+    fn serve(data: &'static [u8], ctype: &'static [u8]) -> Response<Body> {
+        Response::builder()
+        .header("Content-Type", ctype)
+        .body(data.into())
+        .unwrap()
+    }
+
+    #[allow(non_upper_case_globals)]
+    mod ctypes {
+        pub const css: &[u8] = b"text/css";
+        pub const html: &[u8] = b"text/html";
+        pub const ico: &[u8] = b"image/x-icon";
+        pub const js: &[u8] = b"application/javascript";
+        pub const svg: &[u8] = b"image/svg+xml";
+        pub const ttf: &[u8] = b"font/ttf";
+        pub const woff: &[u8] = b"font/woff";
+        pub const woff2: &[u8] = b"font/woff2";
+    }
+}
 
