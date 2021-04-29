9d4b2742489d8905f3ed045d0e0d10eec692c778
Martin Hoffmann <martin@nlnetlabs.nl>
2021-04-29 14:23:19 +0200

    Fix test.

--- src/collector/rrdp.rs
+++ src/collector/rrdp.rs
@@ -2121,7 +2121,7 @@ mod test {
             "c3ab8ff13720e8ad9047dd39466b3c8974e592c2fa383d4a3960714caef0c4f2";
         let digest = DigestAlgorithm::sha256().digest(data);
         let encoded = RepositoryObject::read_into_ivec(
-            &mut data.clone()
+            &mut data.clone(), None,
         ).unwrap();
 
         let hash = RepositoryObject::decode_hash(
