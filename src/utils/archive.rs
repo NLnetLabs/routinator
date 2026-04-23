8d1ba1078373fb3020404f69a01e93ec3d3f2ffa
Koen <koen@nlnetlabs.nl>
2026-04-23 14:50:16 +0200

    Make Clippy happy

--- src/utils/archive.rs
+++ src/utils/archive.rs
@@ -190,7 +190,7 @@ impl<Meta> Archive<Meta> {
         }
 
         // Step 3. Check them objects.
-        objects.sort_by(|left, right| left.0.cmp(&right.0));
+        objects.sort_by_key(|left| left.0);
 
         for window in objects.windows(2) {
             if window[1].0 != window[0].0 + window[0].1 {
@@ -538,7 +538,7 @@ impl<Meta: ObjectMeta> Archive<Meta> {
         if candidates.is_empty() {
             return Ok(None)
         }
-        candidates.sort_by(|left, right| left.0.size.cmp(&right.0.size));
+        candidates.sort_by_key(|left| left.0.size);
         Ok(candidates.first().copied())
     }
 
@@ -1207,6 +1207,7 @@ impl ArchiveStats {
     /// Prints the stats to stdout.
     ///
     /// Uses a two space indent.
+    #[allow(clippy::manual_checked_ops)]
     pub fn print(self) {
         println!("  object count: {}", self.object_count);
         if self.object_count > 0 {
